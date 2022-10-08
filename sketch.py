69ea4a88b77b79c110faa71d52e20e3e3f7fb5de
Krishna Agarwal <34711794+KrishnaAgarwal16@users.noreply.github.com>
2022-10-08 11:01:07 +0530

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('D:/DataScience/OpenCV Sketch/Maa.JPG')
+img = cv2.imread('D:/DataScience/OpenCV Sketch/Test_Image.JPG')
 
 cv2.imshow('',img)
 
@@ -23,6 +23,6 @@ final_img = dodgeV2(img_gray, img_smoothing)
 
 cv2.imshow('',final_img)
 
-img_new = cv2.imwrite('D:/DataScience/OpenCV Sketch/Maa sketch.JPG',final_img)
+img_new = cv2.imwrite('D:/DataScience/OpenCV Sketch/Test_Image_Sketch.JPG',final_img)
 
 print('Done')
